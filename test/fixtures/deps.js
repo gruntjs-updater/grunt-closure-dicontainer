812189e6e4f653dd0d04133e672c5748254fb50e
Daniel Steigerwald <daniel@steigerwald.cz>
2013-12-14 23:57:42 -0800

    Update fixtures deps.

--- test/fixtures/deps.js
+++ test/fixtures/deps.js
@@ -2,5 +2,4 @@ goog.addDependency('../../../../test/fixtures/app.js', ['App'], []);
 goog.addDependency('../../../../test/fixtures/bla.js', ['xyz.Bla'], []);
 goog.addDependency('../../../../test/fixtures/foo.js', ['xyz.Foo'], []);
 goog.addDependency('../../../../test/fixtures/router.js', ['app.Router'], []);
-goog.addDependency('../../../../tmp/default_options_dicontainer.js', ['app.diContainer'], ['App', 'app.Router', 'goog.Disposable']);
 goog.addDependency('../../../../tmp/default_options_dicontainer.js', ['app.DiContainer'], ['App', 'app.Router']);
